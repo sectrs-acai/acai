1d4424737b2fb2ea822eaf4e804ce6a031fe14bf
hi@abertschi.ch <hi@abertschi.ch>
2023-01-09 12:15:44 +0100

    build: update sync tools

--- scripts/abertschi/autosync_thready.sh
+++ scripts/abertschi/autosync_thready.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-sync_dir=$script_dir/..
+sync_dir=$script_dir/../..
 
 host=bean@192.33.93.250
 target=/home/bean
 ignore="$script_dir/.sync-ignore"
-
 
 rsync -v --filter=":- $ignore" -a $sync_dir $host:$target
 while inotifywait -r $script_dir; do
--- scripts/abertschi/run_fvp_ssh_thready.sh
+++ scripts/abertschi/run_fvp_ssh_thready.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $(git rev-parse --show-toplevel)/env.sh
 
-ssh -X bean@192.33.93.250 /home/bean/scripts-sidechannel/scripts/run_fvp.sh
+ssh -X bean@192.33.93.250 /home/bean/trusted-periph/scripts/run_fvp.sh
