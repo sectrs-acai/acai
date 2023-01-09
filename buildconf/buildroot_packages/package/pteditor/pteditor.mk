badb68dec5e61714203c9c60dc9ed7e22a6fd1b5
hi@abertschi.ch <hi@abertschi.ch>
2023-01-09 17:53:51 +0100

    ptedit: make static lib

--- buildconf/buildroot_packages/package/pteditor/pteditor.mk
+++ buildconf/buildroot_packages/package/pteditor/pteditor.mk
@@ -12,25 +12,27 @@ define PTEDITOR_INSTALL_STAGING_CMDS
 
     $(INSTALL) -D -m 0644 $(@D)/ptedit_header.h $(STAGING_DIR)/usr/include/pteditor/ptedit_header.h
 	$(INSTALL) -D -m 0644 $(@D)/ptedit.h $(STAGING_DIR)/usr/include/pteditor/ptedit.h
-	$(INSTALL) -D -m 0655 $(@D)/libptedit.so* $(STAGING_DIR)/usr/lib
+	$(INSTALL) -D -m 0655 $(@D)/lib* $(STAGING_DIR)/usr/lib
 endef
 
 define PTEDITOR_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0655 $(@D)/module/*.ko '$(TARGET_DIR)'
-    $(INSTALL) -D -m 0655 $(@D)/libptedit.so* $(TARGET_DIR)/usr/lib
+    $(INSTALL) -D -m 0655 $(@D)/lib* $(TARGET_DIR)/usr/lib
 endef
 
 define PTEDITOR_BUILD_CMDS
 	# XXX: apply a patch to build a shared library
 	cd $(@D) && patch -N Makefile $(THIS_DIR)/shared-lib.patch
 
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) header lib.ptedit
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) header libptedit.a libptedit.so
 endef
 
 
 define PTEDITOR_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0655 $(@D)/*.out '$(TARGET_DIR)' || true
 	$(INSTALL) -D -m 0655 $(@D)/*.o '$(TARGET_DIR)' || true
+	$(INSTALL) -D -m 0655 $(@D)/*.a '$(TARGET_DIR)' || true
+	$(INSTALL) -D -m 0655 $(@D)/*.so '$(TARGET_DIR)' || true
 endef
 
 $(eval $(kernel-module))
