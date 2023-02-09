f913c93de3040767e1d6245ca865fd5d2002e6f2
hi@abertschi.ch <hi@abertschi.ch>
2023-02-09 10:57:30 +0100

    fpga/escape: do not build escape in rootfs

--- buildconf/linux-host/setup.sh
+++ buildconf/linux-host/setup.sh
@@ -38,7 +38,7 @@ function do_compile {
     cd $BUILDROOT_DIR
     env -u LD_LIBRARY_PATH \
         time make BR2_EXTERNAL=$BR2_EXTERNAL BR2_JLEVEL=$BR2_JLEVEL  O=$BUILDROOT_OUTPUT_DIR \
-        linux-rebuild fvp_escape_host-rebuild host-pteditor-rebuild all
+        linux-rebuild host-pteditor-rebuild all
 }
 
 
