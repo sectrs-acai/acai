88b6d57471531ff713daa48c546499d5eab49e1d
hi@abertschi.ch <hi@abertschi.ch>
2023-03-13 17:27:31 +0100

    build: run realm

--- run_realm.sh
+++ run_realm.sh
@@ -3,4 +3,5 @@
 root=./tmp-sync/tfa-unmod-realm-ready
 
 set -x
-nice -20 ./$root/lkvm run --realm --disable-sve -c 1 -m 256 -k $root/../Image -i assets/rootfs-realm.cpio --debug --9p "/,host0" -p "fvp_escape_loop fvp_escape_on ip=off"
+# --vfio-pci "0000:00:00.0"
+nice -20 ./$root/lkvm run --realm  --disable-sve -c 1 -m 256 -k $root/../snapshots/Image-cca -i assets/rootfs-realm.cpio --debug --9p "/,host0" -p "fvp_escape_loop fvp_escape_on ip=off" --dump-dtb /tmp/lkvm.dtb
