17b4f8b10b1d1eed36b6f0b24f17cb481e3d1462
hi@abertschi.ch <hi@abertschi.ch>
2023-04-06 23:15:48 +0200

    realm: run with 1gb

--- run_realm.sh
+++ run_realm.sh
@@ -4,4 +4,8 @@ root=assets/snapshots
 
 set -x
 # set fvp_escape_off to disable fvp escape tagging
-nice -20 ./$root/lkvm run --realm  --disable-sve -c 1 -m 256 -k $root/Image-cca -i $root/rootfs-realm.cpio --debug --9p "/,host0" -p "fvp_escape_loop fvp_escape_on ip=off" --dump-dtb /tmp/lkvm.dtb
+# --dump-dtb /tmp/lkvm.dtb
+# --debug
+
+nice -20 ./$root/lkvm run --realm  --disable-sve -c 8 -m 1000 \
+    -k $root/Image-cca -i $root/rootfs.realm.cpio --9p "/,host0" -p "fvp_escape_loop fvp_escape_on ip=off"
